83ae92a75999f49ea51634deb1e020ee9f76a38c
Quispe Sierra Alex Jeanpier <188002085+drax3l@users.noreply.github.com>
2026-03-03 10:44:18 -0500

    realizar cambios en templates

--- webstarter/templates.py
+++ webstarter/templates.py
@@ -1,19 +1,43 @@
-from pathlib import Path
+class Templates:
 
-class ProjectStructure:
+    @staticmethod
+    def html(title: str):
+        return f"""<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{title}</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
 
-    def __init__(self, name: str):
-        self.project_path = Path(name)
+    <header>
+        <h1>{title}</h1>
+    </header>
 
-    def create_folders(self):
-        folders = [
-            self.project_path,
-            self.project_path / "css",
-            self.project_path / "js",
-            self.project_path / "assets" / "img"
-        ]
+    <main>
+        <p>Proyecto generado automáticamente con Python 🚀</p>
+        <button onclick="saludar()">Haz clic</button>
+    </main>
 
-        for folder in folders:
-            folder.mkdir(parents=True, exist_ok=True)
+    <script src="js/script.js"></script>
+</body>
+</html>
+"""
 
-        print("✅ Estructura creada correctamente")
+    @staticmethod
+    def css():
+        return """body {
+    font-family: Arial, sans-serif;
+    background-color: #f4f4f4;
+    text-align: center;
+}
+"""
+
+    @staticmethod
+    def js():
+        return """function saludar() {
+    alert("Proyecto generado correctamente 🚀");
+}
+"""
